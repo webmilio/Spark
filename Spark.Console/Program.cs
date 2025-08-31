6143c41047c6f673feb81c76fbec7c639869786f
webmilio <webmilio@users.noreply.github.com>
2025-08-31 13:46:41 +0200

    Add project files.

---
+++ Spark.Console/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Spark.Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
